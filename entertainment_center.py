51d2500f30bf4c37085ed861f5e03fe1399fc503
ape101 <qzhujw@gmail.com>
2015-10-27 12:11:41 -0400

    Fixing code styles

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,26 +1,30 @@
 import media
 import fresh_tomatoes
 
-#List of movies
-movies=[]
+# List of movies
+movies = []
 
-#Add movies to the list
-movies.append(media.Movie("Forest Gump", 
-	"https://www.movieposter.com/posters/archive/main/41/MPW-20583",
-	"www.youtube.com/watch?v=uPIEn0M8su0",
-	"Tom Hank, Robin Wright"))
+# Add movies to the list
+poster_name = "https://www.movieposter.com/posters/archive/main/41/MPW-20583"
+movies.append(media.Movie("Forest Gump",
+                          poster_name,
+                          "www.youtube.com/watch?v=uPIEn0M8su0",
+                          "Tom Hank, Robin Wright"))
 
+poster_name = "https://www.movieposter.com/posters/archive/main/16/MPW-8295"
+actors = "Elijah Wood, Viggo Mortensen, Orlando Bloom, Cate Blanchett"
 movies.append(media.Movie("Lord Of the Rings",
-	"https://www.movieposter.com/posters/archive/main/16/MPW-8295",
-	"https://www.youtube.com/watch?v=Pki6jbSbXIY",
-	"Elijah Wood, Viggo Mortensen, Orlando Bloom, Cate Blanchett"))
+                          poster_name,
+                          "https://www.youtube.com/watch?v=Pki6jbSbXIY",
+                          actors))
 
+poster_name = ("https://upload.wikimedia.org/wikipedia/en/c/ce/"
+               "A_river_runs_through_it_cover.jpg")
 movies.append(media.Movie("A River Runs Through It",
-	"https://upload.wikimedia.org/wikipedia/en/c/ce/A_river_runs_through_it_cover.jpg",
-	"https://www.youtube.com/watch?v=5Z7yeXtBQMU",
-	"Brad Pitt"))
+                          poster_name,
+                          "https://www.youtube.com/watch?v=5Z7yeXtBQMU",
+                          "Brad Pitt"))
 
 
-
-#Open movie list in browser
+# Open movie list in browser
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,21 +1,22 @@
 import webbrowser
 
+
 class Movie():
 	"""Movie class.
 
-	Describes attributes of a movie.
+    Describes attributes of a movie.
 
-	Attributes:
-		title: movie title.
-		poster_image_url: url for movie poster poster_image.
-		trailer_youtube_url: url for movie trailer.
-		movie_stars: string of stars in the movie.
-	"""
+    Attributes:
+        title (str): movie title.
+        poster_image_url (str): url for movie poster poster_image.
+        trailer_youtube_url (str): url for movie trailer.
+        movie_stars (str): string of stars in the movie.
+    """
 
-	# class constructor
-	def __init__(self, movie_title, poster_image_url, trailer_youtube_url, movie_stars):
-		#instance variables
-		self.title = movie_title	
+	def __init__(
+			self, movie_title, poster_image_url,
+			trailer_youtube_url, movie_stars):
+		self.title = movie_title
 		self.poster_image_url = poster_image_url
 		self.trailer_youtube_url = trailer_youtube_url
 		self.movie_stars = movie_stars
